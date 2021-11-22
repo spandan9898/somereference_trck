58c7790eb4c2cb9acdc35589e5f1ee129129bf52
Ruhan Khandakar <khandakarruhan@gmail.com>
2021-11-22 13:09:18 +0530

    db connection setup with mongodb

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 require("dotenv").config();
 
 const server = require("./server");
+const db = require("./src/connector/database");
 
-const bluedartApp = require("./src/apps/bluedart");
-const delhiveryApp = require("./src/apps/delhivery");
+/**
+ * Initialize Database
+ */
+db.initDB((err, _db) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(`${_db.s.options.dbName} DB connected`);
+  }
+});
+
+// const bluedartApp = require("./src/apps/bluedart");
+
+// const delhiveryApp = require("./src/apps/delhivery");
 
 server.createServer();
