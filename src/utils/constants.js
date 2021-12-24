ef4b5938efadb9de000691f7fa945696cf69a0fe
spandan98 <spandan.mishra@pickrr.com>
2021-12-24 13:47:01 +0530

    constants rvt

--- src/utils/constants.js
+++
@@ -1,7 +0,0 @@
-const BLOCKED_IPS = ["54.179.209.160"];
-const ALLOWED_IPS = ["13.233.11.82", "13.233.62.47", "65.0.101.215", "15.206.206.191"];
-
-module.exports = {
-  BLOCKED_IPS,
-  ALLOWED_IPS,
-};
