8a29a1cf5f31348b1e989816583bfd6b6f49d238
Ruhan Khandakar <khandakarruhan@gmail.com>
2022-01-12 22:57:40 +0530

    feat: update elk for status data

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,34 @@
+const {
+  ELASTICO_ID,
+  ELASTICO_USERNAME,
+  ELASTICO_PASSWORD,
+  STAGING_ELASTICO_ID,
+  STAGING_ELASTICO_USERNAME,
+  STAGING_ELASTICO_PASSWORD,
+} = process.env;
+
 const HOST_NAMES = {
   PULL_DB: "PULL_DB",
   REPORT_DB: "REPORT_DB",
+};
+
+const ELK_INSTANCE_NAMES = {
+  PROD: {
+    name: "PROD",
+    config: {
+      elasticoid: ELASTICO_ID,
+      username: ELASTICO_USERNAME,
+      password: ELASTICO_PASSWORD,
+    },
+  },
+  STAGING: {
+    name: "STAGING",
+    config: {
+      elasticoid: STAGING_ELASTICO_ID,
+      username: STAGING_ELASTICO_USERNAME,
+      password: STAGING_ELASTICO_PASSWORD,
+    },
+  },
 };
 
 const WEBHOOK_USER_CACHE_KEY_NAME = "webhookUser";
@@ -8,4 +36,5 @@ const WEBHOOK_USER_CACHE_KEY_NAME = "webhookUser";
 module.exports = {
   HOST_NAMES,
   WEBHOOK_USER_CACHE_KEY_NAME,
+  ELK_INSTANCE_NAMES,
 };
