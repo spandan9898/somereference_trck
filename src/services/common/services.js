64558da5e68363fdd369d90acfa1645fadb2594a
Ruhan Khandakar <khandakarruhan@gmail.com>
2022-01-14 09:38:07 +0530

    feat: updating order type in elk

--- src/services/common/services.js
+++ src/services/common/services.js
@@ -1,23 +1,29 @@
 const get = require("lodash/get");
+const moment = require("moment");
 
 const logger = require("../../../logger");
 const { elkDataUpdate } = require("./elk");
+const { getOrderType } = require("./helpers");
 
 /**
- *
+ * @param trackingDoc -> tracking document(same as DB document)
+ * @desc preapre status and status time and order type and then update ELK
  */
 const updateStatusELK = async (trackingDoc, elkClient) => {
   try {
     const trackingId = trackingDoc.tracking_id;
     const currentStatusTime = get(trackingDoc, "status.current_status_time") || "NA";
     const currentStatusType = get(trackingDoc, "status.current_status_type") || "NA";
+    const orderType = getOrderType(trackingDoc);
+
     await elkDataUpdate({
       upsert: true,
       elkClient,
       id: trackingId,
       doc: {
-        current_status_time: currentStatusTime,
+        current_status_time: moment(currentStatusTime).subtract(330, "minutes").toDate(),
         current_status_type: currentStatusType,
+        order_type: orderType,
         timestamp: new Date(),
       },
     });
