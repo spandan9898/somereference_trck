0fbce6ed4d3f229ad13cc9aba985ef0f058ccf68
Ruhan Khandakar <khandakarruhan@gmail.com>
2022-01-21 10:40:19 +0530

    feat: Handle Same AWB Race Condition

--- src/services/common/services.js
+++ src/services/common/services.js
@@ -3,11 +3,12 @@ const moment = require("moment");
 
 const logger = require("../../../logger");
 const { NEW_STATUS_TO_OLD_MAPPING } = require("../../apps/webhookClients/common/constants");
+const { getObject, setObject } = require("../../utils");
 const { elkDataUpdate } = require("./elk");
 
 /**
  * @param trackingDoc -> tracking document(same as DB document)
- * @desc preapre current status, current status time and order type and then update ELK
+ * @desc prepare current status, current status time and order type and then update ELK
  */
 const updateStatusELK = async (trackingDoc, elkClient) => {
   try {
@@ -28,6 +29,45 @@ const updateStatusELK = async (trackingDoc, elkClient) => {
   }
 };
 
+/**
+ *
+ * @param {*} preparedDict
+ * @desc fetching processCount, default value is 0
+ */
+const getTrackingIdProcessingCount = async ({ awb }) => {
+  try {
+    const cacheData = (await getObject(awb)) || {};
+    const { processCount = 0 } = cacheData;
+    return processCount;
+  } catch (error) {
+    logger.error("getTrackingIdProcessingCount", error);
+    return 0;
+  }
+};
+
+/**
+ *
+ * @param {*} preparedDict
+ * @desc update processCount based on provided type, default type is "add". i.e increase value by 1
+ */
+const updateTrackingProcessingCount = async ({ awb }, type = "add") => {
+  try {
+    const cacheData = (await getObject(awb)) || {};
+    let { processCount = 0 } = cacheData;
+    if (type === "add") {
+      processCount += 1;
+    } else {
+      processCount = !processCount ? 0 : processCount - 1;
+    }
+    cacheData.processCount = processCount;
+    await setObject(awb, cacheData);
+  } catch (error) {
+    logger.error("updateTrackingProcessingCount", error);
+  }
+};
+
 module.exports = {
   updateStatusELK,
+  getTrackingIdProcessingCount,
+  updateTrackingProcessingCount,
 };
