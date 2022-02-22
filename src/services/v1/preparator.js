899bc13c1c061fa3f089744cfc21e36ae0e41078
Ruhan Khandakar <khandakarruhan@gmail.com>
2022-02-22 15:39:24 +0530

    [BACKFILL]: V1

--- src/services/v1/preparator.js
+++ src/services/v1/preparator.js
@@ -20,17 +20,17 @@ const prepareTrackDictForV1 = (trackData) => {
       ? moment(trackData?.status?.current_status_time)
           .add(330, "minutes")
           .format("DD-MM-YYYY HH:mm")
-      : "",
+      : null,
     track_info: trackData?.status?.current_status_body,
     track_location: trackData?.status?.current_status_location,
     received_by: trackData?.status?.received_by,
     pickup_time: pickupTime
       ? moment(pickupTime).add(330, "minutes").format("DD-MM-YYYY HH:mm")
-      : "",
+      : null,
     EDD: trackData?.edd_stamp
       ? moment(trackData?.edd_stamp).add(330, "minutes").format("DD-MM-YYYY HH:mm")
-      : "",
-    pickrr_status: NEW_STATUS_TO_OLD_MAPPING[scanType],
+      : null,
+    pickrr_status: NEW_STATUS_TO_OLD_MAPPING[scanType] || scanType,
     pickrr_sub_status_code:
       trackData?.pickrr_sub_status_code ||
       _.get(trackData, "track_arr[0].pickrr_sub_status_code", ""),
