0866d44572e284e1dd80b835df488196287a5b70
aditya <aditya@pickrr.com>
2022-03-08 18:48:02 +0530

    fix

--- src/apps/dtdc/services.js
+++ src/apps/dtdc/services.js
@@ -62,7 +62,7 @@ const prepareDtdcData = (dtdcDict) => {
 
     const statusDatetime = `${dtdcDict.shipmentStatus.strActionDate} ${dtdcDict.shipmentStatus.strActionTime}`;
     let statusDate = moment(statusDatetime, "DDMMYYYY hhm");
-    statusDate = statusDatetime.isValid()
+    statusDate = statusDate.isValid()
       ? moment(statusDatetime, "DDMMYYYY hhm").format("YYYY-MM-DD HH:mm:ss")
       : moment(new Date()).format("YYYY-MM-DD HH:mm:ss");
     if (scanType.scan_type === "PP") {
