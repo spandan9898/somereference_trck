0d91b4cfe183a27739a9ecc15bcf141416557110
Ruhan Khandakar <khandakarruhan@gmail.com>
2022-03-25 11:07:15 +0530

    feat: delhivery pulled data preparation

--- src/apps/delhivery/services.js
+++ src/apps/delhivery/services.js
@@ -4,6 +4,8 @@ const { PICKRR_STATUS_CODE_MAPPING } = require("../../utils/statusMapping");
 const {
   DELHIVERY_NSL_CODE_TO_STATUS_TYPE_MAPPER,
   DELHIVERY_REVERSE_MAPPER,
+  DELHIVERY_DOCS_MAPPER,
+  DELHIVERY_MAPPER,
 } = require("./constant");
 
 /*
@@ -122,4 +124,101 @@ const prepareDelhiveryData = (delhiveryDict) => {
   }
 };
 
-module.exports = { prepareDelhiveryData };
+/**
+ *
+ * @desc Preparing pickrr dict from delhivery pulled payload
+ * @delhiveryDict {
+      pickUpDate: '2022-03-24T22:21:41.581000',
+      ScanDateTime: '2022-03-24T22:21:41.581000',
+      ScanType: 'UD',
+      Scan: 'Manifested',
+      StatusDateTime: '2022-03-24T22:21:41.581000',
+      ScannedLocation: 'HQ (Haryana)',
+      Instructions: 'Consignment Manifested',
+      StatusCode: 'X-UCI',
+      receivedBy: '',
+      edd: '',
+      event: 'pull',
+      courierName: 'delhivery_air',
+      awbNumber: '7420187003175'
+    }
+ */
+const prepareDelhiveryPulledData = (delhiveryDict) => {
+  const pickrrDelhiveryDict = {
+    awb: "",
+    scan_type: "",
+    scan_datetime: "",
+    track_info: "",
+    track_location: "",
+    received_by: "",
+    pickup_datetime: "",
+    EDD: "",
+    pickrr_status: "",
+    pickrr_sub_status_code: "",
+    courier_status_code: "",
+  };
+
+  const {
+    ScanType,
+    StatusDateTime,
+    ScannedLocation,
+    Instructions,
+    StatusCode,
+    receivedBy,
+    edd,
+    courierName,
+    awbNumber,
+    pickUpDate,
+  } = delhiveryDict || {};
+
+  if (!awbNumber) {
+    return {
+      err: "Tracking ID not available",
+    };
+  }
+  const mapperString = `${StatusCode}_${ScanType}`;
+  let scanType = "";
+
+  if (courierName === "delhivery_reverse") {
+    const mapper = DELHIVERY_REVERSE_MAPPER[mapperString.toLowerCase()];
+    if (mapper) {
+      scanType = mapper.scan_type;
+      pickrrDelhiveryDict.pickrr_sub_status_code = mapper.pickrr_sub_status_code;
+    }
+  } else if (courierName === "delhivery_docs") {
+    const mapper = DELHIVERY_DOCS_MAPPER[mapperString.toLowerCase()];
+    if (mapper) {
+      scanType = mapper.scan_type;
+      pickrrDelhiveryDict.pickrr_sub_status_code = mapper.pickrr_sub_status_code;
+    }
+  } else {
+    const mapper = DELHIVERY_MAPPER[mapperString.toLowerCase()];
+    if (mapper) {
+      scanType = mapper.scan_type;
+      pickrrDelhiveryDict.pickrr_sub_status_code = mapper.pickrr_sub_status_code;
+    }
+  }
+  if (!scanType) {
+    return {
+      err: `Scan type invalid: ${mapperString}`,
+    };
+  }
+
+  if (pickUpDate || scanType === "PP") {
+    const pickupDateTime = pickUpDate || StatusDateTime;
+    pickrrDelhiveryDict.pickup_datetime = moment(pickupDateTime).toDate();
+  }
+
+  pickrrDelhiveryDict.scan_type = scanType === "UD" ? "NDR" : scanType;
+  pickrrDelhiveryDict.track_location = ScannedLocation;
+  pickrrDelhiveryDict.track_info = Instructions;
+  pickrrDelhiveryDict.scan_datetime = moment(StatusDateTime).toDate();
+  pickrrDelhiveryDict.received_by = receivedBy || "";
+  pickrrDelhiveryDict.EDD = edd ? moment(edd).toDate() : "";
+  pickrrDelhiveryDict.awb = awbNumber;
+  pickrrDelhiveryDict.courier_status_code = mapperString;
+  pickrrDelhiveryDict.pickrr_status = PICKRR_STATUS_CODE_MAPPING[scanType];
+
+  return pickrrDelhiveryDict;
+};
+module.exports = { prepareDelhiveryData, prepareDelhiveryPulledData };
