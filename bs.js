564613ba1571351a568d282883636679c7312641
Ruhan Khandakar <khandakarruhan@gmail.com>
2022-04-01 17:37:03 +0530

    refactor: backfilling script

--- bs.js
+++ bs.js
@@ -1,18 +1,27 @@
+/* eslint-disable prefer-const */
 const yargs = require("yargs/yargs");
 const { hideBin } = require("yargs/helpers");
+const moment = require("moment");
+
 const startProcess = require("./scripts/reportBackfill");
 
 const { argv } = yargs(hideBin(process.argv));
 
 /**
  *
- * @desc by default, the type will be "v1". only two types are being supported right now, "report", "v1"
+ * @desc by default, the type will be "v1". only two types are being supported right now, "report", "v1",
+ * pFor = process for
  */
 const main = () => {
-  const { authToken, endDate, startDate, limit, type = "v1" } = argv;
+  let { authToken, endDate, startDate, limit, type = "v1", pFor = "db" } = argv;
 
-  if (!["report", "v1", "elk"].includes(type)) {
+  if (!["report", "v1", "elk", "all"].includes(type)) {
     throw new Error("Type must be either 'v1' or 'report'");
+  }
+
+  if (pFor === "db") {
+    startDate = moment().subtract(3, "days").format("DD-MM-YYYY");
+    endDate = moment().format("DD-MM-YYYY");
   }
 
   startProcess({ authToken, endDate, startDate, limit, type });
