4b40c3c24100fe9d632fdf6c015849eda4f019d3
Rishab Dawar <rishab.dawar@pickrr.com>
2022-04-04 14:59:11 +0530

    minor changes

--- scripts/handlers.js
+++ scripts/handlers.js
@@ -1,7 +1,6 @@
 const fs = require("fs");
 const { pipeline } = require("stream");
 const util = require("util");
-const { COURIER_PARENT_TO_CHILD_MAPPER } = require("../src/utils/constants");
 
 const pump = util.promisify(pipeline);
 const startProcess = require("./reportBackfill");
@@ -13,30 +12,40 @@ const startProcess = require("./reportBackfill");
  * @returns
  */
 exports.backfillHandler = async function backfillHandler(req, reply) {
-  const data = await req.file();
-  const body = data.fields;
+  try {
+    const data = await req.file();
+    const body = data.fields;
 
-  const authToken = body?.auth_token?.value || null;
+    const authToken = body?.auth_token?.value || null;
 
-  const filePath = `${__dirname}/csv_data.csv`;
-  const destination = fs.createWriteStream(filePath);
-  if (data) {
-    await pump(data.file, destination);
+    const filePath = `${__dirname}/csv_data_${new Date()}.csv`;
+    const destination = fs.createWriteStream(filePath);
+    if (data) {
+      await pump(data.file, destination);
+    }
+    const startDate = body?.start_date?.value || null;
+    const endDate = body?.end_date?.value || null;
+    const type = body?.backfill_type?.value || "";
+    let typeList = [];
+    if (type) {
+      typeList = type.split(",");
+    }
+    const limit = body?.limit?.value || 2000;
+
+    await startProcess({
+      authToken,
+      endDate,
+      startDate,
+      limit,
+      type: typeList,
+      csvSavefilepath: filePath,
+    });
+
+    fs.unlink(filePath, (err) => {
+      if (err) throw err;
+    });
+    return reply.code(200).send({ success: true });
+  } catch (error) {
+    return reply.code(200).send({ success: false, err: error.message });
   }
-  const startDate = body?.start_date?.value || null;
-  const endDate = body?.end_date?.value || null;
-  const type = body?.backfill_type?.value || "v1"; // default type v1
-  const courierUsed = body?.courier_used?.value || "";
-  const courierUsedToChildCourierList = COURIER_PARENT_TO_CHILD_MAPPER[courierUsed.toUpperCase()];
-
-  await startProcess({
-    authToken,
-    endDate,
-    startDate,
-    limit: 2000,
-    type,
-    csvSavefilepath: filePath,
-  }); // remove await
-
-  return reply.code(200).send({ success: true });
 };
