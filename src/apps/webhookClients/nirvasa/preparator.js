f3694d902b74aa770604bf3eb099d5d88f2705df
spandan98 <spandan.mishra@pickrr.com>
2022-04-04 14:24:06 +0530

    webhook nirvasa

--- src/apps/webhookClients/nirvasa/preparator.js
+++ src/apps/webhookClients/nirvasa/preparator.js
@@ -28,19 +28,19 @@ const prepareNirvasaWebhookData = (trackObj) => {
     };
     const { status } = trackObj;
     preparedWebhookData.awb = trackObj.tracking_id || trackObj.courier_tracking_id;
-    const statusTime = trackObj.status.current_status_time;
+    const statusTime = trackObj?.status?.current_status_time;
     const statusType = status?.current_status_type;
     preparedWebhookData.datetime = moment(statusTime).isValid()
       ? moment(statusTime).add(330, "minute").format("YYYY-MM-DD HH:MM:SS")
       : "";
     preparedWebhookData.edd = moment(trackObj.edd_stamp).isValid()
-      ? moment(statusTime).add(330, "minute").format("YYYY-MM-DD")
+      ? moment(trackObj.edd_stamp).add(330, "minute").format("YYYY-MM-DD")
       : "";
     preparedWebhookData.status = NIRVASA_STATUS_MAPPER[statusType] || "";
     preparedWebhookData.reason_code = status.current_status_type || "";
-    preparedWebhookData.location = status?.current_status_location || "";
+    preparedWebhookData.location = status.current_status_location || "";
     preparedWebhookData.order_number = trackObj.pickrr_order_id || trackObj.client_order_id;
-    preparedWebhookData.city = trackObj?.info?.to_city || "";
+    preparedWebhookData.city = trackObj.info.to_city || "";
     preparedWebhookData.remarks = status.current_status_body || "";
     preparedWebhookData.product_type = trackObj.info?.cod_amount ? "cod" : "prepaid";
     return preparedWebhookData;
