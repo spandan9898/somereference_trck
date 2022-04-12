36fe8e1c63bcb08744109a6c122437ee93a82d94
spandan98 <spandan.mishra@pickrr.com>
2022-04-12 14:32:29 +0530

    delhivery push

--- src/apps/delhivery/consumer.js
+++ src/apps/delhivery/consumer.js
@@ -2,10 +2,11 @@
 const kafka = require("../../connector/kafka");
 const {
   TOTAL_TOPIC_COUNT,
+  DELHIVERY_PUSH_GROUP_NAME,
+  DELHIVERY_PUSH_TOPIC_NAME,
+  DELHIVERY_PULL_GROUP_NAME,
+  DELHIVERY_PULL_TOPIC_NAME,
   PUSH_CONSUMER_GROUP_NAME,
-  PULL_CONSUMER_GROUP_NAME,
-  PULL_CONSUMER_TOPIC_NAME,
-  PULL_CONSUMER_PARTITION_COUNT,
 } = require("./constant");
 const { KafkaMessageHandler } = require("../../services/common");
 const logger = require("../../../logger");
@@ -15,7 +16,8 @@ const logger = require("../../../logger");
  */
 const initialize = async () => {
   const pushConsumer = kafka.consumer({ groupId: PUSH_CONSUMER_GROUP_NAME });
-  const pullConsumer = kafka.consumer({ groupId: PULL_CONSUMER_GROUP_NAME });
+  const pullConsumer = kafka.consumer({ groupId: DELHIVERY_PULL_GROUP_NAME });
+  const pushConsumerInstance = kafka.consumer({ groupId: DELHIVERY_PUSH_GROUP_NAME });
 
   const totalTopicsCount = new Array(TOTAL_TOPIC_COUNT).fill(1);
   const consumersWithMultiTopics = totalTopicsCount.map(async (_, index) => {
@@ -27,14 +29,17 @@ const initialize = async () => {
       logger.error("Delhivery Initialize error -->", error);
     }
   });
+  await pushConsumerInstance.connect();
+  await pushConsumerInstance.subscribe({ topic: DELHIVERY_PUSH_TOPIC_NAME, fromBeginning: false });
 
   await pullConsumer.connect();
   await pullConsumer.subscribe({
-    topic: PULL_CONSUMER_TOPIC_NAME,
+    topic: DELHIVERY_PULL_TOPIC_NAME,
     fromBeginning: false,
   });
 
   return {
+    pushConsumerInstance,
     consumersWithMultiTopics,
     pullConsumer,
   };
@@ -44,11 +49,11 @@ const initialize = async () => {
  *
  * Listening kafka consumer
  */
-const listener = async (consumer, isPartition) => {
+const listener = async (consumer, partitionsCount) => {
   try {
     await consumer.run({
       autoCommitInterval: 60000,
-      partitionsConsumedConcurrently: isPartition ? PULL_CONSUMER_PARTITION_COUNT : 1,
+      partitionsConsumedConcurrently: partitionsCount,
       eachMessage: (consumedPayload) => {
         const courierName =
           consumedPayload.topic === "delhivery_pull" ? "delhivery_pull" : "delhivery";
