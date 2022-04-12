8083f8883247d8ca6c3b244d1c4874379ee771cd
spandan98 <spandan.mishra@pickrr.com>
2022-04-12 14:42:59 +0530

    xbs removed old logic

--- src/apps/xpressbees/consumer.js
+++ src/apps/xpressbees/consumer.js
@@ -2,33 +2,16 @@
 const kafka = require("../../connector/kafka");
 const { KafkaMessageHandler } = require("../../services/common");
 const logger = require("../../../logger");
-const {
-  XBS_PARTITION_COUNT,
-  XBS_TOPICS_COUNT,
-  XBS_PUSH_TOPIC_NAME,
-  XBS_PUSH_GROUP_NAME,
-} = require("./constant");
+const { XBS_PARTITION_COUNT, XBS_PUSH_TOPIC_NAME, XBS_PUSH_GROUP_NAME } = require("./constant");
 
 /**
  * Initialize consumer and subscribe to topics
  */
 const initialize = async () => {
-  const consumer = kafka.consumer({ groupId: "xbs-group" });
   const pushConsumerInstance = kafka.consumer({ groupId: XBS_PUSH_GROUP_NAME });
-  const topicsCount = new Array(XBS_TOPICS_COUNT).fill(1);
-  const topicConsumerInstances = topicsCount.map(async (_, index) => {
-    try {
-      await consumer.connect();
-      await consumer.subscribe({ topic: `xbs_${index}`, fromBeginning: false });
-      return consumer;
-    } catch (error) {
-      logger.error("XBS Initialize Error", error);
-    }
-  });
   await pushConsumerInstance.connect();
   await pushConsumerInstance.subscribe({ topic: XBS_PUSH_TOPIC_NAME, fromBeginning: false });
   return {
-    topicConsumerInstances,
     pushConsumerInstance,
   };
 };
--- src/apps/xpressbees/index.js
+++ src/apps/xpressbees/index.js
@@ -3,19 +3,8 @@ const { initialize, listener } = require("./consumer");
 
 (async () => {
   try {
-    const { topicConsumerInstances, pushConsumerInstance } = await initialize();
+    const { pushConsumerInstance } = await initialize();
 
-    topicConsumerInstances.forEach((consumer) => {
-      consumer
-        .then((res) => {
-          if (res) {
-            listener(res);
-          }
-        })
-        .catch((error) => {
-          logger.error("XBS Consumer Initialize Error", error);
-        });
-    });
     listener(pushConsumerInstance, true);
   } catch (error) {
     logger.error("XBS Consumer Error", error);
