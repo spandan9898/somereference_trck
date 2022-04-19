b55c51030a65b5744f2dec86849059673eac0f50
Ruhan Khandakar <khandakarruhan@gmail.com>
2022-04-19 16:38:41 +0530

    refactor: V1 Update

--- src/services/v1/index.js
+++ src/services/v1/index.js
@@ -1,8 +1,12 @@
+const sendDataToEventBridge = require("../../connector/eventBridge");
 const { prepareTrackDictForV1 } = require("./preparator");
 const producerConnection = require("../../utils/producerConnection");
 const { produceData } = require("../../utils/kafka");
 const { TOPIC_NAME } = require("./constants");
 const { KAFKA_INSTANCE_CONFIG } = require("../../utils/constants");
+
+const { V1_EVENT_BRIDGE_SOURCE, V1_EVENT_BRIDGE_DETAIL_TYPE, V1_EVENT_BRIDGE_BUS_NAME } =
+  process.env;
 
 /**
  * @param {*} trackData
@@ -12,6 +16,13 @@ const sendTrackDataToV1 = async (trackData) => {
   const trackDict = prepareTrackDictForV1(trackData);
 
   const producerInstance = await producerConnection.connect(KAFKA_INSTANCE_CONFIG.PROD.name);
+
+  sendDataToEventBridge({
+    source: V1_EVENT_BRIDGE_SOURCE,
+    detailType: V1_EVENT_BRIDGE_DETAIL_TYPE,
+    data: trackDict,
+    eventBusName: V1_EVENT_BRIDGE_BUS_NAME,
+  });
 
   const messages = [
     {
