4793c0d5a61af98a1bf3da08422a4061809bb413
spandan98 <spandan.mishra@pickrr.com>
2022-05-19 12:31:55 +0530

    smartr pull partition

--- src/apps/smartr/index.js
+++ src/apps/smartr/index.js
@@ -1,6 +1,6 @@
 const logger = require("../../../logger");
 const { initialize, listener } = require("./consumer");
-const { PUSH_PARTITION_COUNT } = require("./constant");
+const { PULL_PARTITION_COUNT } = require("./constant");
 
 /**
  *
@@ -8,7 +8,7 @@ const { PUSH_PARTITION_COUNT } = require("./constant");
 (async () => {
   try {
     const { pullConsumer } = await initialize();
-    listener(pullConsumer, PUSH_PARTITION_COUNT);
+    listener(pullConsumer, PULL_PARTITION_COUNT);
   } catch (error) {
     logger.error("Smartr Consumer Error", error);
   }
