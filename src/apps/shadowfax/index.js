85f2b808b189132c24ed1c4b32e597ec02a26ed8
spandan98 <spandan.mishra@pickrr.com>
2022-06-23 07:27:33 +0000

    shadowfax

--- src/apps/shadowfax/index.js
+++ src/apps/shadowfax/index.js
@@ -5,10 +5,10 @@ const { initialize, listener } = require("./consumer");
 (async () => {
   try {
     const { pushConsumer, pullConsumer } = await initialize();
-    if (process.env.CONSUME_PULL_EVENTS) {
+    if (process.env.CONSUME_PULL_EVENTS === "true") {
       listener(pullConsumer, PULL_PARTITION_COUNT);
     }
-    if (process.env.CONSUME_PUSH_EVENTS) {
+    if (process.env.CONSUME_PUSH_EVENTS === "true") {
       listener(pushConsumer, PUSH_PARTITION_COUNT);
     }
   } catch (error) {
