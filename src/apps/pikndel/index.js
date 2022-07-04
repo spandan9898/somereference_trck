53d850c75690e2488537b671f20c32dc28d39a3b
spandan98 <spandan.mishra@pickrr.com>
2022-07-04 12:29:15 +0000

    pikndel

---
+++ src/apps/pikndel/index.js
@@ -0,0 +1,15 @@
+const logger = require("../../../logger");
+const { initialize, listener } = require("./consumer");
+const { PUSH_PARTITION_COUNT } = require("./constant");
+
+/**
+ *
+ */
+(async () => {
+  try {
+    const { pushConsumer } = await initialize();
+    listener(pushConsumer, PUSH_PARTITION_COUNT);
+  } catch (error) {
+    logger.error("Pikndel Consumer Error", error);
+  }
+})();
