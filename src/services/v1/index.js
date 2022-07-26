f93be9d7bc45a75453a34d5cd31e2f9df7f2a8cd
spandan98 <spandan.mishra@pickrr.com>
2022-07-26 12:34:09 +0000

    pickup servcie

--- src/services/v1/index.js
+++ src/services/v1/index.js
@@ -32,14 +32,15 @@ const sendTrackDataToV1 = async (trackData) => {
       "64a61f6d3c302d2c478adc888aa20d58791587",
     ];
     const trackDict = prepareTrackDictForV1(trackData);
+
+    // For Pickup Service
+
     sendDataToEventBridge({
       source: V1_EVENT_BRIDGE_SOURCE,
       detailType: V1_NEW_EVENT_BRIDGE_DETAIL_TYPE,
       data: trackDict,
       eventBusName: V1_EVENT_BRIDGE_BUS_NAME,
     });
-
-    // For Pickup Service
 
     if (
       ["OFP", "PPF", "OP", "OM", "OC"].includes(trackData?.status?.current_status_type) ||
