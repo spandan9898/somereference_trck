553f9962ac562a3e898490f13efee31fc90aa458
spandan98 <spandan.mishra@pickrr.com>
2022-08-01 21:57:08 +0000

    smartr

--- src/apps/xpressbees/consumer.js
+++ src/apps/xpressbees/consumer.js
@@ -1,12 +1,7 @@
 /* eslint-disable consistent-return */
 const kafkaInstance = require("../../connector/kafka");
 const { KafkaMessageHandler } = require("../../services/common");
-const {
-  PUSH_TOPIC_NAME,
-  PUSH_GROUP_NAME,
-  PULL_GROUP_NAME,
-  PULL_TOPIC_NAME,
-} = require("./constant");
+const { PUSH_TOPIC_NAME, PUSH_GROUP_NAME } = require("./constant");
 const { KAFKA_INSTANCE_CONFIG } = require("../../utils/constants");
 const logger = require("../../../logger");
 
@@ -17,14 +12,10 @@ const initialize = async () => {
   const kafka = kafkaInstance.getInstance(KAFKA_INSTANCE_CONFIG.PROD.name);
 
   const pushConsumer = kafka.consumer({ groupId: PUSH_GROUP_NAME });
-  const pullConsumer = kafka.consumer({ groupId: PULL_GROUP_NAME });
   await pushConsumer.connect();
   await pushConsumer.subscribe({ topic: PUSH_TOPIC_NAME, fromBeginning: false });
-  await pullConsumer.connect();
-  await pullConsumer.subscribe({ topic: PULL_TOPIC_NAME, fromBeginning: false });
   return {
     pushConsumer,
-    pullConsumer,
   };
 };
 
