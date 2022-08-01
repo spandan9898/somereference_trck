1f9602f584c312150b1aa48369ad1b734f17742f
spandan98 <spandan.mishra@pickrr.com>
2022-08-01 21:42:50 +0000

    smartr

--- src/apps/bluedart/index.js
+++ src/apps/bluedart/index.js
@@ -1,12 +1,11 @@
 const logger = require("../../../logger");
 const { initialize, listener } = require("./consumer");
-const { PUSH_PARTITION_COUNT, PULL_PARTITION_COUNT } = require("./constant");
+const { PUSH_PARTITION_COUNT } = require("./constant");
 
 (async () => {
   try {
-    const { pushConsumer, pullConsumer } = await initialize();
+    const { pushConsumer } = await initialize();
     listener(pushConsumer, PUSH_PARTITION_COUNT);
-    listener(pullConsumer, PULL_PARTITION_COUNT);
   } catch (error) {
     logger.error("Bluedart Consumer Error", error);
   }
