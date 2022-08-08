3ca2f05cd05feb105d0c30ea3709977096e0e6ca
spandan98 <spandan.mishra@pickrr.com>
2022-08-08 11:12:18 +0000

    smartr

--- src/services/report/helpers.js
+++ src/services/report/helpers.js
@@ -135,6 +135,20 @@ const findLostDate = (trackArr, latestStatus) => {
  * @param {*} trackArr
  * @returns
  */
+const findQCFailureReason = (trackArr) => {
+  for (let i = 0; i < trackArr.length; i += 1) {
+    if (trackArr[i].scan_type === "QCF") {
+      return trackArr[i].scan_status;
+    }
+  }
+
+  return null;
+};
+
+/**
+ *
+ * @param {*} trackArr
+ */
 const findLatestNDRDetails = (trackArr) => {
   for (let i = 0; i < trackArr.length; i += 1) {
     if (trackArr[i].scan_type === "NDR") {
