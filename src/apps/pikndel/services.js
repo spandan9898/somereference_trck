2860168b94a52fabda2e45cc65c68d78cfa9a513
aditya <aditya@pickrr.com>
2022-08-08 14:30:30 +0530

    [INTERNAL]: pikndel pull prep changes

--- src/apps/pikndel/services.js
+++ src/apps/pikndel/services.js
@@ -101,6 +101,27 @@ const preparePikNDelData = (payload) => {
  *
  * @param {*} pikndelDict
  * @returns pickrrDict
+ * {
+  pod_no: "PD0078836",
+  reported_on: "2022-07-19 19:16:59",
+  short_code: "DLD",
+  activity: "DELIVERED",
+  reason_code: "",
+  reason: "",
+  google_location:
+    "C3/4, near Pardeswar Dham Temple, Block C3, Keshav Puram, Tri Nagar, New Delhi, Delhi 110035, India",
+  lat: "28.6891425",
+  lng: "77.1574486",
+  order_date: "2022-07-18 12:16:04",
+  expected_delivery_date: "2022-07-19 21:00:00",
+  fe: {
+    fe_name: "Padam Singh",
+    fe_mobile_no: "8447748501",
+  },
+  event : "pull",
+  trackingId : "",
+  isReverse : false
+}
  */
 const preparePulledPikndelData = (pikndelDict) => {
   const pickrrDict = {
@@ -118,42 +139,50 @@ const preparePulledPikndelData = (pikndelDict) => {
   };
 
   try {
-    const mapperString = _.get(pikndelDict, "short_code", "").toString();
+    const {
+      short_code: shortCode = "",
+      reason_code: reasonCode = "",
+      reported_on: reportedOn = "",
+      expected_delivery_date: expectedDeliveryDate = "",
+      reason = "",
+      activity = "",
+      pod_no: podNo = "",
+      google_location: googleLocation = "",
+      fe,
+    } = pikndelDict;
 
-    const statusMappedData = PIKNDEL_STATUS_MAPPER[mapperString.toLowerCase()];
-
-    if (!statusMappedData) {
-      return { err: "No Scan Type Found" };
-    }
-
-    pickrrDict.awb = pikndelDict?.pod_no;
-    if (!pickrrDict.awb) {
+    if (!podNo) {
       logger.error("Pikndel Pull Preparator --> No AWB found", pikndelDict);
       return {};
     }
+    let mapperString = shortCode.toString();
+    if (mapperString === "CAN" || mapperString === "PEN") {
+      mapperString = reasonCode ? `${mapperString}_${reasonCode}` : mapperString;
+    }
+    const scanType = PIKNDEL_STATUS_MAPPER[mapperString.toLowerCase()];
+    if (!scanType) {
+      return { err: "No Scan Type Found" };
+    }
+    pickrrDict.awb = podNo;
 
-    pickrrDict.scan_type = statusMappedData.scan_type;
-
-    pickrrDict.pickrr_sub_status_code = statusMappedData.pickrr_sub_status_code;
-
-    let eventDateTime = _.get(pikndelDict, "reported_on", "");
-
+    pickrrDict.scan_type = scanType?.scan_type;
+    pickrrDict.pickrr_sub_status_code = scanType?.pickrr_sub_status_code;
+    let eventDateTime = reportedOn;
     eventDateTime = moment(eventDateTime).isValid
       ? moment(eventDateTime).format("YYYY-MM-DD HH:mm:ss")
       : moment().format("YYYY-MM-DD HH:mm:ss");
-
-    let eddStamp = _.get(pikndelDict, "expected_delivery_date", "");
-
+    let eddStamp = expectedDeliveryDate;
     eddStamp = moment(eddStamp).isValid() ? moment(eddStamp).format("YYYY-MM-DD HH:mm:ss") : "";
-
     pickrrDict.EDD = eddStamp;
-
     pickrrDict.scan_datetime = eventDateTime;
-
     pickrrDict.courier_status_code = mapperString;
-
+    pickrrDict.track_info = reason ? `${activity} - ${reason}` : `${activity}`;
+    pickrrDict.track_location = googleLocation;
     if (pickrrDict.scan_type === "PP") {
       pickrrDict.pickup_datetime = eventDateTime;
+    }
+    if (pickrrDict.scan_type === "DL") {
+      pickrrDict.received_by = fe?.fe_name ? fe.fe_name : "";
     }
 
     pickrrDict.pickrr_status = PICKRR_STATUS_CODE_MAPPING[pickrrDict.scan_type];
