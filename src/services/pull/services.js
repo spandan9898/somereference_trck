ba7064cbdb416f3f556b246c8aebebb6e810c581
aditya <aditya@pickrr.com>
2022-08-16 16:52:21 +0530

    [INTERNAL]: added logs to pull events,removed POC

--- src/services/pull/services.js
+++ src/services/pull/services.js
@@ -69,10 +69,6 @@ const updateCacheTrackArray = async ({ trackArray, awb, trackingDocument }) => {
  */
 const redisCheckAndReturnTrackData = async (preparedTrackData, isFromPulled) => {
   const trackObj = { ...preparedTrackData };
-  if (preparedTrackData?.awb === "1747388") {
-    logger.info("went into handling for pidge awb");
-    return trackObj;
-  }
   const isExists = await checkAwbInCache({ trackObj, updateCacheTrackArray, isFromPulled });
   if (isExists) {
     return false;
