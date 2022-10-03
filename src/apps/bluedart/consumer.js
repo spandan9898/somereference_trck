402c66da087f0c53d81589841ebd2862d13418fd
aditya <aditya@pickrr.com>
2022-10-03 14:02:02 +0530

    [temp]: disable bluedart pull

--- src/apps/bluedart/consumer.js
+++ src/apps/bluedart/consumer.js
@@ -4,8 +4,9 @@ const { KafkaMessageHandler } = require("../../services/common");
 const {
   PUSH_GROUP_NAME,
   PUSH_TOPIC_NAME,
-  PULL_TOPIC_NAME,
-  PULL_GROUP_NAME,
+
+  // PULL_TOPIC_NAME,
+  // PULL_GROUP_NAME,
 } = require("./constant");
 const logger = require("../../../logger");
 const { KAFKA_INSTANCE_CONFIG } = require("../../utils/constants");
@@ -18,15 +19,21 @@ const initialize = async () => {
   const kafka = kafkaInstance.getInstance(KAFKA_INSTANCE_CONFIG.PROD.name);
 
   const pushConsumer = kafka.consumer({ groupId: PUSH_GROUP_NAME });
-  const pullConsumer = kafka.consumer({ groupId: PULL_GROUP_NAME });
+
+  // const pullConsumer = kafka.consumer({ groupId: PULL_GROUP_NAME });
+
   await pushConsumer.connect();
-  await pullConsumer.connect();
+
+  // await pullConsumer.connect();
+
   await pushConsumer.subscribe({ topic: PUSH_TOPIC_NAME, fromBeginning: false });
-  await pullConsumer.subscribe({ topic: PULL_TOPIC_NAME, fromBeginning: false });
+
+  // await pullConsumer.subscribe({ topic: PULL_TOPIC_NAME, fromBeginning: false });
 
   return {
     pushConsumer,
-    pullConsumer,
+
+    // pullConsumer,
   };
 };
 
